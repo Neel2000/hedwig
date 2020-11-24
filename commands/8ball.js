8e8ff6bf2f652cabe3311aa677336a3f87f48e62
Neel2000 <74824878+Neel2000@users.noreply.github.com>
2020-11-24 21:03:07 +0530

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,16 +1,17 @@
 const { MessageEmbed  } = require('discord.js');
 module.exports = {
       name: '8ball',
-	description: 'QA',
-	args: true,
-     execute(message, client,args) => {
-     let reason = args.join(' ');
-    if (reason.length < 1) return message.channel.send('You did not give the bot a question');
-    var ball = ['It is certain.','No doubt about it.','No chance.','Maybe, time will tell.','No way.','Concentrate and try again.', ' As I see it, yes', 'Outlook good', 'Most likely', 'Better not tell you now', 'My sources say no', 'Signs point to yes', 'Yes definitely', 'It is decidedly so', 'As I see it, yes', 'My sources say no', 'My sources say no', 'Outlook not so good', 'Very doubtful'];
-    const embed = new MessageEmbed()
-    .setColor(0x00A2E8)
-    .addField("You asked", reason)
-    .addField("Hedwig says", ball[Math.floor(Math.random () * ball.length)])
-    .setThumbnail("http://www.pngmart.com/files/3/8-Ball-Pool-Transparent-PNG.png")
-    message.channel.send({embed})
-}
+      description: 'QA',
+      args: true,
+      execute(message, client,args) {
+            let reason = args.join(' ');
+	    if (reason.length < 1) return message.channel.send('You did not give the bot a question');
+	    var ball = ['It is certain.','No doubt about it.','No chance.','Maybe, time will tell.','No way.','Concentrate and try again.', ' As I see it, yes', 'Outlook good', 'Most likely', 'Better not tell you now', 'My sources say no', 'Signs point to yes', 'Yes definitely', 'It is decidedly so', 'As I see it, yes', 'My sources say no', 'My sources say no', 'Outlook not so good', 'Very doubtful'];
+	    const embed = new MessageEmbed()
+	    .setColor(0x00A2E8)
+	    .addField("You asked", reason)
+	    .addField("Hedwig says", ball[Math.floor(Math.random () * ball.length)])
+	    .setThumbnail("http://www.pngmart.com/files/3/8-Ball-Pool-Transparent-PNG.png")
+	    message.channel.send({embed})
+      },
+};
