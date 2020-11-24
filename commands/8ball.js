34f93a2e494114362746e44c581413a97b6b9ad3
Neel2000 <74824878+Neel2000@users.noreply.github.com>
2020-11-24 21:00:52 +0530

    Update 8ball.js

--- commands/8ball.js
+++ commands/8ball.js
@@ -1,5 +1,9 @@
 const { MessageEmbed  } = require('discord.js');
-exports.run = (client, message, args) => {
+module.exports = {
+      name: '8ball',
+	description: 'QA',
+	args: true,
+     execute(message, client,args) => {
      let reason = args.join(' ');
     if (reason.length < 1) return message.channel.send('You did not give the bot a question');
     var ball = ['It is certain.','No doubt about it.','No chance.','Maybe, time will tell.','No way.','Concentrate and try again.', ' As I see it, yes', 'Outlook good', 'Most likely', 'Better not tell you now', 'My sources say no', 'Signs point to yes', 'Yes definitely', 'It is decidedly so', 'As I see it, yes', 'My sources say no', 'My sources say no', 'Outlook not so good', 'Very doubtful'];
