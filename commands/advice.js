21f518bd12d3652056225cfbf5168271696d1f03
Neel2000 <74824878+Neel2000@users.noreply.github.com>
2020-11-24 21:14:13 +0530

    Update and rename snekfetch.js to advice.js

--- commands/advice.js
+++ commands/advice.js
@@ -5,7 +5,7 @@ module.exports = {
 	args: true,
 	execute(message, client,args) {
 		 try {
-        		const { body } = await snekfetch.get('http://api.adviceslip.com/advice');
+        		const { body } = snekfetch.get('http://api.adviceslip.com/advice');
         		message.channel.send(JSON.parse(body.toString()).slip.advice);
         	} catch (err) {
         		message.channel.send(`An error occurred: \`${err.message}\`. Try again later!`);
