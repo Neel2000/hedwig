19243f167756fd2160fb2c6a83e042683e2f16a4
Neel2000 <74824878+Neel2000@users.noreply.github.com>
2020-11-25 09:17:08 +0530

    Update index.js

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ client.on('message', message => {
 	setTimeout(() => timestamps.delete(message.author.id), cooldownAmount);
 
 	try {
-		command.execute(message, client,args);
+		await command.execute(message, client,args);
 	} catch (error) {
 		console.error(error);
 		message.reply('there was an error trying to execute that command!');
